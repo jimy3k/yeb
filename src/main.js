394662e865b69b7d14e7de598bed991c9ce613d9
jimy3k <jimy3k@126.com>
2022-09-20 16:40:38 +0800

    axios拦截器

--- src/main.js
+++ src/main.js
@@ -4,20 +4,12 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
-Vue.use(axios, VueAxios)
-
-//全局配置
-Vue.prototype.$axios = axios
-axios.defaults.baseURL = '/api'
 
 new Vue({
-  axios,
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
